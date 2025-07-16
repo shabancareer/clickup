04a12eb6fc17c6b7c5c834b44f8c99bc415cd0c7
m.shaban <shabankml@gmail.com>
2025-07-16 20:44:13 +0500

    Git update and test

--- apps/nest-app/back-end/src/app.service.ts
+++ apps/nest-app/back-end/src/app.service.ts
@@ -4,6 +4,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello():{message:string} {
-    return { message: 'hello from backend Service' };
+    return { message: 'hello from backend Service updated' };
   }
 }
